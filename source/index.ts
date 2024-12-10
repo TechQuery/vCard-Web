4a619ef99945bd3416904df1e2adda50a6f924ae
South Drifted <shiy2008@gmail.com>
2024-12-10 20:07:52 +0000

    [refactor] transform DOM-Renderer v1 codes to DOM-Renderer v2 & WebCell v3 [migrate] replace Material Design Lite with MDUI [migrate] replace Yarn with PNPM to reduce Disk Usage [migrate] replace Travis CI with GitHub actions [optimize] upgrade to Parcel 2, Husky 9, Lint-Staged 15 & other latest Upstream packages [optimize] update Read Me document

--- source/index.ts
+++ source/index.ts
@@ -1,47 +1,8 @@
-import 'regenerator-runtime/runtime';
-// @ts-ignore
-import { ViewList } from 'dom-renderer';
-// @ts-ignore
-import vCard from 'vcf';
-import { toCanvas } from 'qrcode';
+import { DOMRenderer, VNode } from 'dom-renderer';
+import { configure } from 'mobx';
 
-import { makeVCard } from './utility';
+import './VCardGenerator';
 
-import meta from './meta.yml';
+configure({ enforceActions: 'never' });
 
-const form = document.querySelector('form'),
-    canvas = document.querySelector('main > canvas');
-
-const field_view = new ViewList(form),
-    data = localStorage.getItem('vCard');
-
-field_view.render(meta).then(() => {
-    if (!form) return;
-
-    form.onchange = event => {
-        event.preventDefault();
-        // @ts-ignore
-        const card = makeVCard(form.elements);
-
-        localStorage.setItem('vCard', JSON.stringify(card.toJSON()));
-
-        toCanvas(canvas, card.toString('3.0'));
-    };
-
-    if (!data) return;
-
-    const card = JSON.parse(data);
-
-    for (let item of card[1]) {
-        const field = form.elements[item[0]] as HTMLInputElement;
-
-        if (!field) continue;
-
-        if (['photo', 'logo'].includes(item[0]))
-            item[3] = item[3].split('URI:')[1];
-
-        field.value = item[3];
-    }
-
-    toCanvas(canvas, vCard.fromJSON(card).toString('3.0'));
-});
+new DOMRenderer().render(new VNode({ tagName: 'vcard-generator' }));
